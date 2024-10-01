2b45d49b8f299725f929b77d1f48d09a2a9ff795
thanajong <thanajong@outlook.com>
2024-10-01 13:28:18 +0700

    Ex1.4 courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,56 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+// The main.jsx already know the array 'parts' from the
+// 'App' component which exported to main.jsx already 
+//
+// 'App' passed the 'parts' prop 
+// with the value of array 'parts'
+// to 'Content', and 'Content' is going 
+// to take and use the value through destructuring  
+// 
+// The argument {parts} tells 'Content' to use value from props.parts
+const Content = ( {parts} ) => {
+  return (
+    <div>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
+    </div>
+  )
+}
+
+// The same concept in 'Content' works here too.
+const Total = ( {parts} ) => {
+  return (
+    <p>Total: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <>
-      <h1>{course}</h1>
-      <div>
-        <p>{part1.name} {part1.exercises}</p>
-        <p>{part2.name} {part2.exercises}</p>
-        <p>{part3.name} {part3.exercises}</p>
-      </div>
-      <p>Total: {part1.exercises+part2.exercises+part3.exercises}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </>
   )
 }
