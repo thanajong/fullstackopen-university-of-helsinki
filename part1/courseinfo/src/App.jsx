66f19aa5280e61e97c7887790b7b42430b083b36
thanajong <thanajong@outlook.com>
2024-10-01 17:12:41 +0700

    Ex1.5 courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,56 +1,48 @@
-const Header = (props) => {
+const Header = ( {course} ) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{course.name}</h1>
   )
 }
 
-// The main.jsx already know the array 'parts' from the
-// 'App' component which exported to main.jsx already 
-//
-// 'App' passed the 'parts' prop 
-// with the value of array 'parts'
-// to 'Content', and 'Content' is going 
-// to take and use the value through destructuring  
-// 
-// The argument {parts} tells 'Content' to use value from props.parts
-const Content = ( {parts} ) => {
+const Content = ( {course} ) => {
   return (
     <div>
-      <p>{parts[0].name} {parts[0].exercises}</p>
-      <p>{parts[1].name} {parts[1].exercises}</p>
-      <p>{parts[2].name} {parts[2].exercises}</p>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
     </div>
   )
 }
 
-// The same concept in 'Content' works here too.
-const Total = ( {parts} ) => {
+const Total = ( {course} ) => {
   return (
-    <p>Total: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    <p>Total: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
