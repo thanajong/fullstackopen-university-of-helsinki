ecb666ad68d5dde09d155e01ffdf6bb94502ad3a
thanajong <thanajong@outlook.com>
2024-10-04 10:24:09 +0700

    Ex1.6 unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Header = () => {
+  return (
+    <h1>Give Feedback</h1>
+  )
+}
+
+const FeedbackButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.name}
+    </button>
+  )
+}
+
+const Statistics = ( {f} ) => {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>Good: {f.good}</p>
+      <p>Neutral: {f.neutral}</p>
+      <p>Bad: {f.bad}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+  console.log(feedback)
+
+  const onGoodClick = () => {
+    const newFeedback = {
+      ...feedback,
+      good: feedback.good + 1
+    }
+    setFeedback(newFeedback)
+  }
+
+  const onNeutralClick = () => {
+    const newFeedback = {
+      ...feedback,
+      neutral: feedback.neutral + 1
+    }
+    setFeedback(newFeedback)
+  }
+
+  const onBadClick = () => {
+    const newFeedback = {
+      ...feedback,
+      bad: feedback.bad + 1
+    }
+    setFeedback(newFeedback)
+  }
+
+  return (
+    <>
+      <Header />
+      <FeedbackButton name='good' onClick={onGoodClick} />
+      <FeedbackButton name='neutral' onClick={onNeutralClick} />
+      <FeedbackButton name='bad' onClick={onBadClick} />
+      <Statistics f={feedback}/>
+    </>
+  )
+}
+
+export default App
