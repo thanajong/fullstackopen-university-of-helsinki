4f846980de221b814d2dc8a238c63aec9ebd760a
thanajong <thanajong@outlook.com>
2024-10-12 23:51:21 +0700

    Ex2.4 couseinfo step 9

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -15,45 +15,69 @@ const Sum = ({ parts }) => {
   return <p><b>Total of {total} exercises</b></p>
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Sum parts={course.parts} />
+      {courses.map((course) => {
+        return (
+          <div key={course.id} className="course">
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Sum parts={course.parts} />
+          </div>
+        )
+      })}
     </>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Course courses={courses} />
 }
 
 export default App
