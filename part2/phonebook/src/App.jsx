589fe336f0c3d8ecfcd66a325c70cbeadd366c77
thanajong <thanajong@outlook.com>
2024-10-15 12:12:26 +0700

    Ex2.8 phonebook step 3 fixed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,31 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id:1, name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: "0123456789" }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const onAdd = (event) => {
     //prevent page reloading
     event.preventDefault()
-    for(let i = 0; i < persons.length; i++){
-      if(newName === persons[i].name){
+    for (let i = 0; i < persons.length; i++) {
+      if (newName === persons[i].name) {
         alert(`${newName} is already existed.`)
         return
       }
     }
     const personObject = {
       id: persons.length + 1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     console.log('Add:', newName)
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleChange = (event) => {
+  const handleNewNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNewNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -37,7 +45,13 @@ const App = () => {
           name:
           <input
             value={newName}
-            onChange={handleChange} />
+            onChange={handleNewNameChange} />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNewNumberChange} />
         </div>
         <div>
           <button
@@ -49,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.id}>{person.name}</li>)}
+        {persons.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
