9d56b4834777b727977d65fdfe5cf150c6f4d7dd
thanajong <thanajong@outlook.com>
2024-10-15 13:00:23 +0700

    Ex2.10 phonebook step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,29 +1,47 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: "0123456789" }
-  ])
+const Header = ({header}) => {
+  return <h1><span>{header}</span></h1>
+}
+
+const Filter = ({setFilter}) => {
+  const handleFilterChange = (event) => {
+    console.log(`filter: ${event.target.value}`)
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <span>filter:</span>
+      <input
+        type="filter"
+        onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const Add = ({persons, setPersons}) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-
-  const personsToShow = persons.filter((person) => person.name.includes(filter))
 
   const onAdd = (event) => {
+    
     //prevent page reloading
     event.preventDefault()
+
     for (let i = 0; i < persons.length; i++) {
       if (newName === persons[i].name) {
         alert(`${newName} is already existed.`)
         return
       }
     }
+
     const personObject = {
       id: persons.length + 1,
       name: newName,
       number: newNumber
     }
+
     setPersons(persons.concat(personObject))
     console.log('Add:', newName)
     setNewName('')
@@ -40,21 +58,9 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleFilterChange = (event) => {
-    console.log(`filter: ${event.target.value}`)
-    setFilter(event.target.value)
-  }
-
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <div>
-        <span>filter:</span>
-        <input 
-        type="filter" 
-        onChange={handleFilterChange}/>
-      </div>
-      <h2>Add</h2>
+    <>
+      <h2><span>Add</span></h2>
       <form>
         <div>
           <span>name:</span>
@@ -76,10 +82,34 @@ const App = () => {
           </button>
         </div>
       </form>
-      <h2>Numbers</h2>
+    </>
+  )
+}
+
+const Display = ({persons, filter}) => {
+  const personsToShow = persons.filter((person) => person.name.includes(filter))
+  return (
+    <>
+      <h2><span>Numbers</span></h2>
       <ul>
-        {personsToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
+        {personsToShow.map(person => <li key={person.id}><span>{person.name} {person.number}</span></li>)}
       </ul>
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: "0123456789" }
+  ])
+  const [filter, setFilter] = useState('')
+
+  return (
+    <div>
+      <Header header='Phonebook' />
+      <Filter setFilter={setFilter}/>
+      <Add persons={persons} setPersons={setPersons}/>
+      <Display persons={persons} filter={filter}/>
     </div>
   )
 }
